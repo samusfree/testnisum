022df441aaf3ade4d77cd6cfeb7c389226de823b
Samuel Gonzales <samusfree@SamusMac.local>
2023-06-26 04:07:19 -0500

    first commit

---
+++ src/test/java/com/nisum/testnisum/TestnisumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.testnisum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestnisumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
